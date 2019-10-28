96d475cdc933f8b3f9f8037c5b33d26de782ad47
Your Name <alexsigala87@gmail.com>
2019-10-27 19:57:29 -0700

    palidrom checker works now. need to add an alert for when it isnt a palindrome. it only alerts when its positive

--- public/js/main.js
+++ public/js/main.js
@@ -6,5 +6,24 @@ $(document).ready(function() {
         console.log("clicked");
         let inputText = $(".palBox").val();
         console.log(`Checking to see if ${inputText} is a palindrome`);
+
+        palCheck(inputText);
     });
+    //--------------------FUNCTIONS--------------------------------------------
+    const palCheck = inputText => {
+        let reveresedArray = [];
+        for (i = 0; i < inputText.length; i++) {
+            reveresedArray.push(inputText.charAt(inputText.length - i - 1));
+            console.log(reveresedArray);
+        }
+        let inputReversed = reveresedArray.join("");
+        if (inputText === inputReversed) {
+            console.log("yes");
+            alert("yes that is a palidrome");
+            return true;
+        } else {
+            console.log(`no ${inputReversed} is not the same as ${inputText}`);
+            return false;
+        }
+    };
 });
