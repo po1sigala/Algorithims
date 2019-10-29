72954a7668b2338c9475177a1d2625724918ffbd
Your Name <alexsigala87@gmail.com>
2019-10-28 18:41:54 -0700

    created algorithm to remove duplicates

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log("logic loaded");
-    // run palindrom algo when
+    // run palindrom algo when palbtn clicked
     $(document).on("click", "#palBtn", function() {
         //get the input string to check
         console.log("clicked");
@@ -8,6 +8,38 @@ $(document).ready(function() {
         console.log(`Checking to see if ${inputText} is a palindrome`);
 
         palCheck(inputText);
+    });
+    //run remove duplicates when noDup clicked
+    $(document).on("click", "#noDup", function() {
+        console.log("clicked noDup");
+        //convert the input seperated by spaces into an array.
+        /*[a,c,c,d,f,g,g]
+        0,a
+        1,c
+        2,c
+        3,d
+        4,f
+        5,g
+        6,g
+        */
+        let inputArray = $(".dupBox")
+            .val()
+            .toString()
+            .split(" ");
+        console.log(`initial input as an array is ${inputArray}`);
+        let uniquesOnly = [];
+        for (i = 0; i < inputArray.length; i++) {
+            // filterUnique(inputArray[i], i);
+            //when it is a repeat the index of input will be less than the current i
+            if (inputArray.indexOf(inputArray[i]) === i) {
+                //if its a uniquq push it to the unique array
+                uniquesOnly.push(inputArray[i]);
+                console.log(uniquesOnly);
+            }
+        }
+        alert(
+            ` you entered ${uniquesOnly.length} unique inputs. they are: ${uniquesOnly}`
+        );
     });
     //--------------------FUNCTIONS--------------------------------------------
     const palCheck = inputText => {
@@ -22,8 +54,17 @@ $(document).ready(function() {
             alert("yes that is a palidrome");
             return true;
         } else {
-            console.log(`no ${inputReversed} is not the same as ${inputText}`);
+            alert(`no ${inputReversed} is not the same as ${inputText}`);
             return false;
         }
     };
+    const filterUnique = (input, i) =>
+        inputArray.filter((input, i) => {
+            //when it is a repeat the index of input will be less than the current i
+            if (inputArray.indexOf(input) === i) {
+                //if its a uniquq push it to the unique array
+                uniquesOnly.push(input);
+                console.log(uniquesOnly);
+            }
+        });
 });
